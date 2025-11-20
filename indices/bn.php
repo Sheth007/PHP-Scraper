f4cecd9d32f79ba271640da4e5e88c686efe8a92
Sheth Uday <shethuday505@gmail.com>
2025-11-20 12:58:00 +0530

    new files

---
+++ indices/bn.php
@@ -0,0 +1,42 @@
+<script>
+    function fetchPrice() {
+
+        // fetch('https://etapi.indiatimes.com/et-screener/index-summary?indexId=1913')
+        fetch('https://priceapi.moneycontrol.com/pricefeed/notapplicable/inidicesindia/in%3Bnbx')
+            .then(response => response.json())  // Parse the response as JSON
+            .then(data => {
+                // fetching the 'lastTradedPrice' is directly in the response object
+                const price = data.data.pricecurrent;
+
+                if (price) {
+                    document.getElementById('bn-current-price').innerText = price;
+                    // console.log('Price updated:', price, 'at', new Date().toLocaleTimeString());
+                } else {
+                    console.log('Price data not found!');
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching price:', error);
+                // document.getElementById('current-price').innerText = 'Error loading data';
+            });
+    }
+
+    fetchPrice();
+
+    // uncommnent this below line to fetch the price live
+    setInterval(fetchPrice, 1000);
+
+</script>
+<?php
+include "../mstatus.php";
+?>
+
+<body>
+    <b>
+        <div>Bank Nifty : <span id="bn-current-price">Loading...</span></div>
+    </b>
+
+
+</body>
+
+</html>
